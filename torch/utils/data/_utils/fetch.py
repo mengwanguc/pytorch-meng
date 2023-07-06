5ecef3ad03e946f832adf8cb287c13b16398e463
Gus Waldspurger <gus@waldspurger.com>
2023-07-06 15:28:01 -0700

    logging

--- torch/utils/data/_utils/fetch.py
+++ torch/utils/data/_utils/fetch.py
@@ -21,9 +21,7 @@ class _IterableDatasetFetcher(_BaseDatasetFetcher):
         self.dataset_iter = iter(dataset)
 
     def fetch(self, possibly_batched_index):
-        print("iterative")
         if self.auto_collation:
-            print("")
             data = []
             for _ in possibly_batched_index:
                 try:
@@ -42,9 +40,9 @@ class _MapDatasetFetcher(_BaseDatasetFetcher):
         super(_MapDatasetFetcher, self).__init__(dataset, auto_collation, collate_fn, drop_last)
 
     def fetch(self, possibly_batched_index):
-        print("map")
         if self.auto_collation:
             data = [self.dataset[idx] for idx in possibly_batched_index]
+            print(data[0])
         else:
             data = self.dataset[possibly_batched_index]
         return self.collate_fn(data)
