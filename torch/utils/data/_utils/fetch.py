23b966e53d97a392450da335668da48df8005d9a
Gus Waldspurger <gus@waldspurger.com>
2023-07-06 15:51:05 -0700

    try tuple

--- torch/utils/data/_utils/fetch.py
+++ torch/utils/data/_utils/fetch.py
@@ -45,7 +45,7 @@ class _MapDatasetFetcher(_BaseDatasetFetcher):
         return self.dataset[index]
 
     async def load_many_data(self, indices):
-        return await asyncio.gather([self.load_single_data(idx) for idx in indices])
+        return await asyncio.gather(*(self.load_single_data(idx) for idx in indices))
 
     def fetch(self, possibly_batched_index):
         if self.auto_collation:
