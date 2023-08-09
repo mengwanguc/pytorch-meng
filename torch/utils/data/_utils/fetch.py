15d3244e395e282c79858f8007019b02bc361da0
Gus Waldspurger <gus@waldspurger.com>
2023-08-09 12:47:14 -0700

    better logging

--- torch/utils/data/_utils/fetch.py
+++ torch/utils/data/_utils/fetch.py
@@ -58,5 +58,6 @@ class _MapDatasetFetcher(_BaseDatasetFetcher):
             # Async loader must be run with auto collation.
             assert False
             data = self.dataset[possibly_batched_index]
-        print(all_data)
+        for i, data in enumerate(all_data):
+            print("data {}: {}".format(i, data))
         return [self.collate_fn(data) for data in all_data]
