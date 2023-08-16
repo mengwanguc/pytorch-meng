0ee9bfa7b246f594f007d475b65cd5f5fdb35a57
Gus Waldspurger <gus@waldspurger.com>
2023-08-15 17:47:02 -0700

    move -1 placement

--- torch/utils/data/dataloader.py
+++ torch/utils/data/dataloader.py
@@ -1292,11 +1292,11 @@ class _MultiProcessingDataLoaderIter(_BaseDataLoaderIter):
     def _try_put_index(self):
         assert self._tasks_outstanding < max(self._super_batch_size, self._prefetch_factor) * self._num_workers
 
+        empty = False
         try:
             index = self._next_index()
         except StopIteration:
-            self._index_queues[next(self._worker_queue_idx_cycle)].put(-1)
-            return
+            empty = True
         for _ in range(self._num_workers):  # find the next active worker, if any
             worker_queue_idx = next(self._worker_queue_idx_cycle)
             if self._workers_status[worker_queue_idx]:
@@ -1305,7 +1305,10 @@ class _MultiProcessingDataLoaderIter(_BaseDataLoaderIter):
             # not found (i.e., didn't break)
             return
 
-        self._index_queues[worker_queue_idx].put((self._send_idx, index))
+        if not empty:
+            self._index_queues[worker_queue_idx].put((self._send_idx, index))
+        else:
+            self._index_queues[worker_queue_idx].put(-1)
         self._task_info[self._send_idx] = (worker_queue_idx,)
         self._tasks_outstanding += 1
         self._send_idx += 1
